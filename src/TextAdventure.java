c8045d6558e6daa4ee9ed9df07c138a3c4aa4eac
Ruben <rubentrevino95@gmail.com>
2020-03-25 10:29:12 -0500

    adding methods

--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -21,7 +21,7 @@ public class TextAdventure {
     public static boolean fighting = false;
 
     //this builds the player
-    public void playerOne() {
+    public static void playerOne() {
         maxHealth = 100;
         mana = 100;
         attack = 10;
@@ -33,66 +33,100 @@ public class TextAdventure {
     }
 
     //This will create the goblin enemy method
-        public void goblinEncounter(){
+        public static void goblin(){
             switch (Level) {
                 case 1:
-                    currHealth = 10;
-                    attack = 10;
+                    currEHealth = 10;
+                    enemyAttack = 10;
                     break;
                 case 2:
-                    currHealth = 15;
-                    attack = 15;
+                    currEHealth = 15;
+                    enemyAttack = 15;
                     break;
+                case 3:
+                    currEHealth = 20;
+                    enemyAttack = 20;
             }
         }
 
-        //This is the creation of the fighting method
-        public void fight(){
-        goblinEncounter();
-        fighting = true;
-        while(fighting = true) {
-            System.out.println("You have entered a fight! What will you do?");
+        public static boolean attack() {
+            System.out.println("Enemy Hit!");
+            enemyHealth = enemyHealth - attack;
+            return true;
         }
+        public static boolean run() {
+            System.out.println("You successfully escaped!");
+            return true;
+        }
+
+        //This is the creation of the fighting method
+        public static void fight(){
+        Scanner s = new Scanner(System.in);
+        String action;
+        System.out.println("An enemy is about to attack...What will you do?");
+        System.out.println("Attack[a]   Run[b]");
+        action = s.nextLine();
+
+        goblin();
+        fighting = true;
+
+        while(fighting = true) {
+            if (action.equals('a')) {
+                attack();
+            }
+            if (action.equals('b')) {
+                run();
+            }
+        }
+
     }
+
+////////////////////////////////////////////////////
 
 
     //This is the input at the start of the game.
     public static void main (String[] args){
-        Scanner s = new Scanner(System.in);
+            Scanner s = new Scanner(System.in);
 
-        //Prompts the user to input a new name for their persona
-        System.out.println("Please enter a your name: ");
-        String playerName = s.next();
+            //Prompts the user to input a new name for their persona
+            System.out.println("Please enter your name: ");
+            String playerName = s.next();
 
-        // Informs the user of their name
-        System.out.println(playerName + " Is a great name!");
+            // Informs the user of their name
+            System.out.println(playerName + " Is a great name!");
 
-        //Asks if the user wishes to continue
-        System.out.println("\nWould you like to continue? [y/n]");
-        String yesNo = s.next();
-        if(yesNo.equalsIgnoreCase("y")){
-            System.out.println("Great! Lets get going!");
-        }else{
-            System.out.println("That sucks, see you later then");
-        }
-
-        //The first event for the player
-        System.out.println("You are walking through a forest and come upon two different paths.\n Which path would like to take? [left(l)/right(r)");
-        while(true) {
-            String leftRight = s.next();
-            if (leftRight.equalsIgnoreCase("l")) {
-                System.out.println("Oh no! you ran into a goblin!?");
-                break;
-            } else if (leftRight.equalsIgnoreCase("r")) {
-                System.out.println("You continue to skip down the forest path.");
-                break;
+            //Asks if the user wishes to continue
+            System.out.println("\nWould you like to continue? [y/n]");
+            String yesNo = s.next();
+            if (yesNo.equalsIgnoreCase("y")) {
+                System.out.println("Great! Lets get going!");
             } else {
-                System.out.println("Wrong choice! Try again!");
+                System.out.println("That sucks, see you later then");
             }
+
+            //The first event for the player
+            System.out.println("You are walking through a forest and come upon two different paths.\n Which path would like to take? [left(l)/right(r)");
+            while (true) {
+                String leftRight = s.next();
+                if (leftRight.equalsIgnoreCase("l")) {
+                    System.out.println("Oh no! you ran into a goblin!?");
+                    fight();
+                    break;
+                } else if (leftRight.equalsIgnoreCase("r")) {
+                    System.out.println("You continue to skip down the forest path.");
+                    break;
+                } else {
+                    System.out.println("Wrong choice! Try again!");
+                }
+            }
+
+            System.out.println("A wild goblin appears!");
+            String userInput = s.nextLine();
+
+
+
+
+
         }
 
-        System.out.println("A wild goblin ");
-
-
-    }
 }
